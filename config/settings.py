154c318af5e63a6cf3bc292ce8977bc395b85db3
Femi Ayo-Tubosun <femiayotubosun@gmail.com>
2021-11-05 01:04:22 +0100

    Hid secret key

--- config/settings.py
+++ config/settings.py
@@ -18,9 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$y!8grn0zxz1==&n$n3e03#0_3rj#2gh9&pgib$oi_2hbd2o&r"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
